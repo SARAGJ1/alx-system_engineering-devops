a3bbe5d674ff0d1873c3b5ceeefe815bcff724fe
SARAGJ1 <sara.gj.14.05@gmail.com>
2024-08-20 10:51:15 +0000

    commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
-file { '/etc/php/7.4/apache2/conf.d/20-custom.ini':
-  ensure  => file,
-  content => 'memory_limit = 256M
-              upload_max_filesize = 50M
-              post_max_size = 50M',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-}
+# web stack debugging 3
 
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/php/7.4/apache2/conf.d/20-custom.ini'],
+exec { 'fix_code':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/bin/:/bin/',
 }
